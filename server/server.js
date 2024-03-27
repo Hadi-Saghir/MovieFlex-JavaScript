7094a5c01b1c6d3a8f3484fa64b914a26470d54a
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2024-03-27 06:48:08 +0100

    cors config

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const querystring = require('querystring');
 const bodyParser = require('body-parser');
 const fs = require('fs');
